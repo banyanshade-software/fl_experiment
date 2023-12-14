73a21e2c489b9c71b7f417436f67a54b08bc418e
banyanshade-software <braun.daniel@free.fr>
2023-12-14 21:53:25 +0100

    fixed both parser now available

--- pureMCU/main.ml
+++ pureMCU/main.ml
@@ -28,19 +28,31 @@ module L = MenhirLib.LexerUtil
 
 
 
-let env checkpoint =
+let mienv checkpoint =
   match checkpoint with
   | HI.HandlingError env ->
       env
   | _ ->
       assert false
 
-
-
-let state checkpoint : int =
-  match HI.top (env checkpoint) with
+let mistate checkpoint : int =
+  match HI.top (mienv checkpoint) with
   | Some (HI.Element (s, _, _, _)) ->
       HI.number s
+  | None ->
+      0
+
+let mlenv checkpoint =
+  match checkpoint with
+  | MI.HandlingError env ->
+      env
+  | _ ->
+      assert false
+
+let mlstate checkpoint : int =
+  match MI.top (mlenv checkpoint) with
+  | Some (MI.Element (s, _, _, _)) ->
+      MI.number s
   | None ->
       0
 
@@ -51,8 +63,8 @@ let show text positions =
   |> E.compress
   |> E.shorten 20 
 
-let get text checkpoint i =
-  match HI.get i (env checkpoint) with
+let miget text checkpoint i =
+  match HI.get i (mienv checkpoint) with
   | Some (HI.Element (_, _, pos1, pos2)) ->
       show text (pos1, pos2)
   | None ->
@@ -62,6 +74,18 @@ let get text checkpoint i =
          into the known suffix of the stack. *)
       "???"
 
+let mlget text checkpoint i =
+match MI.get i (mlenv checkpoint) with
+  | Some (MI.Element (_, _, pos1, pos2)) ->
+      show text (pos1, pos2)
+  | None ->
+      (* The index is out of range. This should not happen if [$i]
+         keywords are correctly inside the syntax error message
+         database. The integer [i] should always be a valid offset
+         into the known suffix of the stack. *)
+      "???"
+
+(*
 let fail text buffer (checkpoint : _ HI.checkpoint) =
   (* Indicate where in the input file the error occurred. *)
   let location = L.range (E.last buffer) in
@@ -74,19 +98,60 @@ let fail text buffer (checkpoint : _ HI.checkpoint) =
   (* Show these three components. *)
   Format.eprintf "%s%s%s%!" location indication message;
   exit 1
+*)
 
 
 let succeed _v =
   assert false
 
-
+(*
 let parsechan parsef lexerf l =
     let supplier = HI.lexer_lexbuf_to_supplier lexerf l in
     let buffer, supplier = E.wrap_supplier supplier in
     let checkpoint = Miparser.Incremental.topDecls l.lex_curr_p in
     HI.loop_handle succeed (fail "hoo" buffer) supplier checkpoint
+*)
 
 
+let parsechan_mi l =
+    let parsef = Miparser.topDecls in
+    let lexerf = Indenter.token in
+    let fail text buffer (checkpoint : _ HI.checkpoint) =
+        (* Indicate where in the input file the error occurred. *)
+        let location = L.range (E.last buffer) in
+        (* Show the tokens just before and just after the error. *)
+        let indication = Printf.sprintf "Syntax error %s.\n" (E.show (show text) buffer) in
+        (* Fetch an error message from the database. *)
+        let message = MiParsingErrors.message (mistate checkpoint) in
+        (* Expand away the $i keywords that might appear in the message. *)
+        let message = E.expand (miget text checkpoint) message in
+        (* Show these three components. *)
+        Format.eprintf "%s%s%s%!" location indication message;
+        exit 1 in
+    let supplier = HI.lexer_lexbuf_to_supplier lexerf l in
+    let buffer, supplier = E.wrap_supplier supplier in
+    let checkpoint = Miparser.Incremental.topDecls l.lex_curr_p in
+    HI.loop_handle succeed (fail "hoo" buffer) supplier checkpoint
+
+let parsechan_ml l =
+    let parsef = Mlparser.exp in
+    let lexerf = Mllexer.token in
+    let fail text buffer (checkpoint : _ MI.checkpoint) =
+        (* Indicate where in the input file the error occurred. *)
+        let location = L.range (E.last buffer) in
+        (* Show the tokens just before and just after the error. *)
+        let indication = Printf.sprintf "Syntax error %s.\n" (E.show (show text) buffer) in
+        (* Fetch an error message from the database. *)
+        let message = MiParsingErrors.message (mlstate checkpoint) in
+        (* Expand away the $i keywords that might appear in the message. *)
+        let message = E.expand (mlget text checkpoint) message in
+        (* Show these three components. *)
+        Format.eprintf "%s%s%s%!" location indication message;
+        exit 1 in
+    let supplier = MI.lexer_lexbuf_to_supplier lexerf l in
+    let buffer, supplier = E.wrap_supplier supplier in
+    let checkpoint = Mlparser.Incremental.exp l.lex_curr_p in
+    MI.loop_handle succeed (fail "hoo" buffer) supplier checkpoint
 
 let lexbuf p outchan = (* バッファをコンパイルしてチャンネルへ出力する (caml2html: main_lexbuf) *)
   Id.counter := 0;
@@ -105,16 +170,16 @@ let lexbuf p outchan = (* バッファをコンパイルしてチャンネルへ出力する (caml2htm
 
 let string s = 
     let lex = Lexing.from_string s in
-    let p = parsechan Mlparser.exp Mllexer.token lex in
+    let p = parsechan_ml lex in
     lexbuf p stdout  (* 文字列をコンパイルして標準出力に表示する (caml2html: main_string) *)
 
 (*let lexpar f = if (Filename.extension f) = ".ml" then (Mllexer.token, Mlparser.exp) else (Milexer.token, Miparser.exp)*)
-let lexpar f = if (Filename.extension f) = ".ml" then (Mllexer.token, Mlparser.exp) else (Indenter.token, Miparser.topDecls)
+let lexpar f = if (Filename.extension f) = ".ml" then (Mllexer.token, parsechan_ml) else (Indenter.token, parsechan_mi)
 
 let file_ast f =
   let inchan = open_in f in
-  let (lexerf, parsef) = lexpar f in
-  let p = parsechan parsef lexerf (Lexing.from_channel inchan) in
+  let (lexerf, parsech) = lexpar f in
+  let p = parsech (Lexing.from_channel inchan) in
     p
   
 let file_lex f =
@@ -126,7 +191,7 @@ let file_lex f =
     if t = EOF then r else loop (t :: r )
   in List.rev (loop [])
 
-let lexer = Mllexer.token
+(*let lexer = Mllexer.token*)
 
 let file_lexer f = 
     let inchan = open_in f in
@@ -139,9 +204,9 @@ let file_lexer f =
 let file f = (* ファイルをコンパイルしてファイルに出力する (caml2html: main_file) *)
   Format.printf "extension '%s'\n" (Filename.extension f);
   let inchan = open_in f in
-  let (lexerf, parsef) = if (Filename.extension f) = ".ml" then (Mllexer.token, Mlparser.exp) else (Milexer.token, Miparser.topDecls) in
+  let (lexerf, parsechan) = lexpar f in (*if (Filename.extension f) = ".ml" then (Mllexer.token, Mlparser.exp) else (Milexer.token, Miparser.topDecls) in *)
   let outchan = open_out (f ^ ".s") in
-  let p = parsechan parsef lexerf (Lexing.from_channel inchan) in
+  let p = parsechan (Lexing.from_channel inchan) in
   try
     lexbuf p outchan;
     close_in inchan;
--- pureMCU/main.mli
+++ pureMCU/main.mli
@@ -3,5 +3,5 @@ val string : string -> unit
 val file : string -> unit
 val file_ast : string -> Syntax.t
 val file_lex : string -> Tokens.token list
-val lexer : Lexing.lexbuf -> Tokens.token
+(* val lexer : Lexing.lexbuf -> Tokens.token*)
 val file_lexer : string -> Tokens.token
